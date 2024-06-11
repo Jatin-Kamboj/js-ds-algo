9cea6101c683294ba3e986e6e53d56d8a83ce886
jatin-kamboj <er.jatinkamboj96@gmail.com>
2024-06-11 19:38:45 +0530

    Added imperative and decalaratoive examples

---
+++ advanced-js/inperative-declarative.js
@@ -0,0 +1,11 @@
+// what is Imperative and Declarative 
+
+// Imperative means telling the program what to do and how to do it.
+// Therefore it is the human who is telling the both things to the program
+
+// Declartive
+// Telling the function what to do it but not telling how to do it.
+// Such as forEach loop to iterate over all the elements in an array but not telling the
+// program how to do it.
+// It is the written under the hood in the interperator telling the program how to iterate
+ 
