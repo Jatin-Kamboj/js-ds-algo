c7d548236a32b3f77812062c262a0b6170b7d209
jatin-kamboj <er.jatinkamboj96@gmail.com>
2024-06-12 17:24:02 +0530

    Added example of declarative

--- advanced-js/inperative-declarative.js
+++ advanced-js/inperative-declarative.js
@@ -13,3 +13,14 @@
 // Such as React is decalartive
 // We tell it what to do such as render a text in a component but not telling how to to it
 // It is pre defined under the hood in the virtual DOM
+
+
+/**
+ * Example of declarative code
+ * In below example we tell the program to iterate over an array of (users) [What to do]
+ * But we did not tell the program how to iterate over the array. It is predefined in the 
+ * Javascript engine
+ */
+
+// const users = ["name"]
+// users.forEach((user) =>  console.log("name -> ",name))
