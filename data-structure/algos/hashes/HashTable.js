d43f46fa309d3d49dac314752b7266ef40e8f810
Jatin-Kamboj <er.jatinkamboj96@gmail.com>
2024-10-17 00:39:46 +0530

    Added hash table keys function

--- data-structure/algos/hashes/HashTable.js
+++ data-structure/algos/hashes/HashTable.js
@@ -22,6 +22,21 @@ class HashTable {
 
     return items?.[1];
   }
+
+  keys() {
+    const keysList = [];
+    if (this.data.length === 0) {
+      return keysList;
+    }
+
+    // O(n) Linear time complexity. Traversing all the elements in the hash
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysList.push(this.data[i][0]);
+      }
+    }
+    return keysList;
+  }
 }
 
 const myHashTable = new HashTable(50);
@@ -30,6 +45,8 @@ myHashTable.set("grapes", 10000);
 myHashTable.get("grapes");
 myHashTable.set("apples", 9);
 myHashTable.get("apples");
+const hashKeys = myHashTable.keys();
+console.log("hashKeys :>> ", hashKeys);
 
 // console.log(myHashTable.get("grapes"))
 // console.log(myHashTable.get("apples"))
