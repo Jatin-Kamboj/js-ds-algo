6f3c78e385450a68297ec7f93ca124c98697923b
Jatin-Kamboj <er.jatinkamboj96@gmail.com>
2024-10-24 01:28:01 +0530

    Added prepend in linked list and increased the length by one

--- data-structure/linkedLists/singlyLinkedList.js
+++ data-structure/linkedLists/singlyLinkedList.js
@@ -1,29 +1,37 @@
 class SinglyLinkedList {
   tail = null;
   head = null;
+  length = 0;
 
   constructor(value) {
     this.head = {
       value,
       next: null,
     };
+
+    this.tail = this.head;
+    this.#incrementLength();
+  }
+
+  #incrementLength() {
+    this.length++;
   }
 
   add(value) {
-    if (this.head && !this.tail) {
-      this.tail = {
-        value,
-        next: null,
-      };
+    console.log("this.tail :>> ", this.tail);
+    const newNode = {
+      value,
+      next: null,
+    };
 
-      this.head.next = this.tail;
-    } else {
-      console.log("this.tail :>> ", this.tail);
-      this.tail.next = {
-        value,
-        next: null,
-      };
-    }
+    // This is important
+    // Right now this next points to the previous node so we assigne next
+    // to the new node
+    this.tail.next = newNode;
+
+    // Replace the nee node with the tail as the last node in the list
+    this.tail = newNode;
+    this.#incrementLength();
   }
 
   values() {
@@ -35,14 +43,25 @@ class SinglyLinkedList {
       node = node.next;
       data.push(node.value);
     } while (node.next);
-
+    console.log("data :>> ", data);
     return data;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value,
+      next: this.head,
+    };
+
+    this.head = newNode;
   }
 }
 
 const linkedList = new SinglyLinkedList(10);
 linkedList.add(5);
 linkedList.add(16);
+linkedList.prepend(100);
 linkedList.values();
 
+console.log("Length => ", linkedList.length);
 console.log("linkedList.head :>> ", linkedList.tail);
